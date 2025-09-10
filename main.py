babc2bf5046c8d597b3ea2c07288340f379d62aa
ao587 <2963652496@qq.com>
2025-09-10 20:07:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # 在这个文件下编写代码，题目具体要求见README.md文件
+weight = input("请输入要转换的重量")
+if(weight[-1] == 'g'):
+  exweight = (int(weight[0:-2]))*2.2046
+  print("对应的英制重量为%.3f公斤"%(exweight))
+if(weight[-1] == 'd'):
+  exweight1 = (int(weight[0:-2]))/2.2046
+  print("对应的英制重量为%.3f公斤"%(exweight1))
